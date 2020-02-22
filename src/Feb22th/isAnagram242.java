f7e6437dbb69578b35cbf5058141b19844fb46a8
17602937887 <545192053@qq.com>
2020-02-22 22:47:20 +0800

    Code

--- src/Feb22th/isAnagram242.java
+++ src/Feb22th/isAnagram242.java
@@ -1,21 +1,13 @@
 package Feb22th;
 
+import java.util.Arrays;
+
 public class isAnagram242 {
-    // 存储每个ASCII码出现的次数, 遍历第二个不够则返回false 否则返回true
     public boolean isAnagram(String s, String t) {
-        if(s == null && t == null) return true;
-        if(s == null && t != null) return false;
-        if(s != null && t == null) return false;
-        if(s.length() != t.length()) return false;
-        int[] ans = new int[256];
-        for(int i = 0; i < s.length(); i++){
-            ans[s.charAt(i)]++;
-        }
-        for(int i = 0; i < t.length(); i++){
-            if(--ans[t.charAt(i)] < 0){
-                return false;
-            }
-        }
-        return true;
+        char[] charS = s.toCharArray();
+        char[] charT = t.toCharArray();
+        Arrays.sort(charS);
+        Arrays.sort(charT);
+        return Arrays.equals(charS, charT);
     }
 }
