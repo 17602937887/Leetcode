c3c7a01ba387e5e6f70b48eba272c12613e3fc56
17602937887 <545192053@qq.com>
2020-03-04 23:20:22 +0800

    Code

---
+++ src/Mar4th/distributeCandies575.java
@@ -0,0 +1,18 @@
+package Mar4th;
+
+import java.util.HashSet;
+
+public class distributeCandies575 {
+    public int distributeCandies(int[] candies) {
+        int k = candies.length / 2;
+        HashSet<Integer> a = new HashSet<>();
+        for(int i = 0; i < candies.length; i++){
+            a.add(candies[i]);
+        }
+        if(a.size() >= k){
+            return k;
+        } else {
+            return a.size();
+        }
+    }
+}
